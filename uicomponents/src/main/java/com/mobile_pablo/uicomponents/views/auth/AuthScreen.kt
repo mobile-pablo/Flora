7dc96ced02bdb7ededdc7180093301d70e0821ba
mobile-pablo <pawel.lukasz001@gmail.com>
2022-09-11 17:25:03 +0200

    Add playground module

---
+++ uicomponents/src/main/java/com/mobile_pablo/uicomponents/views/auth/AuthScreen.kt
@@ -0,0 +1,19 @@
+package com.mobile_pablo.uicomponents.views.auth
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun authScreen() {
+    Box(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+
+    }
+}
