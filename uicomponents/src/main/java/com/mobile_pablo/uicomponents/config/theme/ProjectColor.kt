dfb1851c24bb00a619bd5ffc8e016e7d83a5f2fb
mobile-pablo <pawel.lukasz001@gmail.com>
2022-09-11 18:26:53 +0200

    Work on colors by 60-20-10 rule

--- uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/ProjectColor.kt
+++ uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/ProjectColor.kt
@@ -4,15 +4,11 @@ import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 data class ProjectColor(
-    val Color_60 : Color = Color(0xFFFFFFFF),
-    val Color_30 : Color = Color(0xFFFFFFFF),
-    val Color_10 : Color = Color(0xFFFFFFFF),
-    val Paragraph : Color = Color(0xFFFFFFFF),
-    val H1 : Color = Color(0xFFFFFFFF),
-    val H2 : Color = Color(0xFFFFFFFF),
-    val H3 : Color = Color(0xFFFFFFFF),
-    val H4 : Color = Color(0xFFFFFFFF),
-    val H5 : Color = Color(0xFFFFFFFF)
+    val Color_60 : Color = Color(0xFF38A739),
+    val Color_30 : Color = Color(0xFF73C174),
+    val Color_10 : Color = Color(0xFFAFDBAF),
+    val Main : Color = Color(0xFFFFFFFF),
+    val Secondary : Color = Color(0xFF1C2019),
 )
 
 val LocalProjectColor = compositionLocalOf { ProjectColor() }
--- uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/Theme.kt
+++ uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/Theme.kt
@@ -11,30 +11,17 @@ fun FloraTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    // TODO : Add colors
     val colors = if (darkTheme) {
         ProjectColor(
             Color_60 = Color(0xFF38A739),
             Color_30 = Color(0xFF73C174),
             Color_10 = Color(0xFFAFDBAF),
-            Paragraph = Color(0xFF323B2F),
-            H1 = Color(0xFF232920),
-            H2 = Color(0xFF1f241c),
-            H3 = Color(0xFF1c2019),
-            H4 = Color(0xFF181c16),
-            H5 = Color(0xFF151813),
         )
     } else {
         ProjectColor(
             Color_60 = Color(0xFFFF8C00),
             Color_30 = Color(0xFFFFAE4C),
             Color_10 = Color(0xFFFFD199),
-            Paragraph = Color(0xFF323B2F),
-            H1 = Color(0xFF232920),
-            H2 = Color(0xFF1f241c),
-            H3 = Color(0xFF1c2019),
-            H4 = Color(0xFF181c16),
-            H5 = Color(0xFF151813),
         )
     }
 
