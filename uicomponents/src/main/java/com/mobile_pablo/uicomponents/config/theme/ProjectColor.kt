2e461e781f792d6b21ec5dc4c9871e8cecee017f
mobile-pablo <pawel.lukasz001@gmail.com>
2022-09-11 18:23:56 +0200

    Add more colors by 60-20-10 rule

--- uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/ProjectColor.kt
+++ uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/ProjectColor.kt
@@ -8,10 +8,11 @@ data class ProjectColor(
     val Color_30 : Color = Color(0xFFFFFFFF),
     val Color_10 : Color = Color(0xFFFFFFFF),
     val Paragraph : Color = Color(0xFFFFFFFF),
-    val H4 : Color = Color(0xFFFFFFFF),
-    val H3 : Color = Color(0xFFFFFFFF),
-    val H2 : Color = Color(0xFFFFFFFF),
     val H1 : Color = Color(0xFFFFFFFF),
+    val H2 : Color = Color(0xFFFFFFFF),
+    val H3 : Color = Color(0xFFFFFFFF),
+    val H4 : Color = Color(0xFFFFFFFF),
+    val H5 : Color = Color(0xFFFFFFFF)
 )
 
 val LocalProjectColor = compositionLocalOf { ProjectColor() }
--- uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/Theme.kt
+++ uicomponents/src/main/java/com/mobile_pablo/uicomponents/config/theme/Theme.kt
@@ -14,11 +14,27 @@ fun FloraTheme(
     // TODO : Add colors
     val colors = if (darkTheme) {
         ProjectColor(
-            Color_10 = Color(0xFFBB86FC)
+            Color_60 = Color(0xFF38A739),
+            Color_30 = Color(0xFF73C174),
+            Color_10 = Color(0xFFAFDBAF),
+            Paragraph = Color(0xFF323B2F),
+            H1 = Color(0xFF232920),
+            H2 = Color(0xFF1f241c),
+            H3 = Color(0xFF1c2019),
+            H4 = Color(0xFF181c16),
+            H5 = Color(0xFF151813),
         )
     } else {
         ProjectColor(
-            Color_10 = Color(0xFFBB86FC)
+            Color_60 = Color(0xFFFF8C00),
+            Color_30 = Color(0xFFFFAE4C),
+            Color_10 = Color(0xFFFFD199),
+            Paragraph = Color(0xFF323B2F),
+            H1 = Color(0xFF232920),
+            H2 = Color(0xFF1f241c),
+            H3 = Color(0xFF1c2019),
+            H4 = Color(0xFF181c16),
+            H5 = Color(0xFF151813),
         )
     }
 
